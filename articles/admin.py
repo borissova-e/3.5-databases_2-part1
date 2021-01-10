fd8873f43c87cef36b88e5658147fc1d0a1d9232
Elena Borisova <borissova.e@yandex.ru>
2021-01-10 18:36:41 +0300

    Initial commit

---
+++ articles/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Scope, ArticleScope
+
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
+    pass
+
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            form.cleaned_data
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            raise ValidationError('Основным  может быть только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    formset = ArticleScopeInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        ArticleScopeInline
+    ]
---
+++ articles/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.db.models import F
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+
+    topic = models.TextField(verbose_name='Название', unique=True)
+    articles = models.ManyToManyField(Article, blank=True, related_name='scopes', through='ArticleScope')
+
+    def __str__(self):
+        return self.topic
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+        ordering = ['-topic']
+
+
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(Article, related_name='topic_article', on_delete=models.CASCADE)
+    topic = models.ForeignKey(Scope, verbose_name='Раздел', related_name='article_topic', on_delete=models.CASCADE)
+    main = models.BooleanField(verbose_name='Основной', default=False)
+
+    def __str__(self):
+        return f'{self.article} {self.topic} {self.main}'
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематика статей'
+        ordering = ['-main']
